e7b453bfaa23aff372c81086db4bccc93a4c731d
Ebubekir Dogan.MSc AI <ebubekir.dogan@bil.omu.edu.tr>
2024-10-22 19:53:19 +0300

    Shoulder press açıları güncellendi

--- best/fitness-app/exercises/shoulder_press_strategy.py
+++ best/fitness-app/exercises/shoulder_press_strategy.py
@@ -12,9 +12,9 @@ class ShoulderPressStrategy(ExerciseStrategy):
 
     def calculate_angle(self, a, b, c):
         """Üç nokta arasındaki açıyı hesapla."""
-        a = np.array(a)  # Omuz
-        b = np.array(b)  # Dirsek
-        c = np.array(c)  # Bilek
+        a = np.array(a)  # İlk nokta
+        b = np.array(b)  # Ortadaki nokta
+        c = np.array(c)  # Son nokta
         
         radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
         angle = np.abs(radians * 180.0 / np.pi)
@@ -25,18 +25,18 @@ class ShoulderPressStrategy(ExerciseStrategy):
         return angle
 
     def draw_arc(self, image, center, angle, max_angle=140, radius=30):
-            """Belirli bir açıya göre yay çizer."""
-            angle_fraction = angle / max_angle  # Açı oranı
-            end_angle = int(360 * angle_fraction)  # Çizilecek dilimin son açısı
-            
-            # Dilimi çiz
-            cv2.ellipse(image, center, (radius, radius), 0, 0, end_angle, (0, 255, 0), 3)
-            
-            # Açıyı ortada göster
-            cv2.putText(image, f'{int(angle)}', 
-                        (center[0] - 30, center[1] + 5), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2, cv2.LINE_AA)
-            
+        """Belirli bir açıya göre yay çizer."""
+        angle_fraction = angle / max_angle  # Açı oranı
+        end_angle = int(360 * angle_fraction)  # Çizilecek dilimin son açısı
+        
+        # Dilimi çiz
+        cv2.ellipse(image, center, (radius, radius), 0, 0, end_angle, (0, 255, 0), 3)
+        
+        # Açıyı ortada göster
+        cv2.putText(image, f'{int(angle)}', 
+                    (center[0] - 30, center[1] + 5), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2, cv2.LINE_AA)
+
     def perform_exercise(self):
         # MediaPipe bileşenlerini ayarla
         mp_pose = mp.solutions.pose
@@ -72,32 +72,32 @@ class ShoulderPressStrategy(ExerciseStrategy):
                     # Koordinatları al
                     landmarks = results.pose_landmarks.landmark
                     
-                    # Sağ ve sol omuz, dirsek ve bilek koordinatları
-                    right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x * image.shape[1], 
-                                      landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y * image.shape[0]]
-                    right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x * image.shape[1], 
+                    # Belirtilen landmarkların koordinatlarını al
+                    landmark_23 = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x * image.shape[1], 
+                                   landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y * image.shape[0]]
+                    landmark_11 = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x * image.shape[1], 
                                    landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y * image.shape[0]]
-                    right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x * image.shape[1], 
+                    landmark_13 = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x * image.shape[1], 
                                    landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y * image.shape[0]]
                     
-                    left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x * image.shape[1], 
-                                     landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y * image.shape[0]]
-                    left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x * image.shape[1], 
-                                  landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y * image.shape[0]]
-                    left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x * image.shape[1], 
-                                  landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y * image.shape[0]]
+                    landmark_24 = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x * image.shape[1], 
+                                   landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y * image.shape[0]]
+                    landmark_12 = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x * image.shape[1], 
+                                   landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y * image.shape[0]]
+                    landmark_14 = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x * image.shape[1], 
+                                   landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y * image.shape[0]]
                     
-                    # Sağ ve sol açıyı hesapla
-                    right_angle = self.calculate_angle(right_shoulder, right_elbow, right_wrist)
-                    left_angle = self.calculate_angle(left_shoulder, left_elbow, left_wrist)
-                    
+                    # Açıları hesapla
+                    right_angle = self.calculate_angle(landmark_23, landmark_11, landmark_13)
+                    left_angle = self.calculate_angle(landmark_24, landmark_12, landmark_14)
+
                     # Her iki omuz için dilim çizimi
-                    right_shoulder_center = (int(right_shoulder[0]), int(right_shoulder[1]))
+                    right_shoulder_center = (int(landmark_23[0]), int(landmark_23[1]))
                     self.draw_arc(image, right_shoulder_center, right_angle, max_angle)
                     
-                    left_shoulder_center = (int(left_shoulder[0]), int(left_shoulder[1]))
+                    left_shoulder_center = (int(landmark_24[0]), int(landmark_24[1]))
                     self.draw_arc(image, left_shoulder_center, left_angle, max_angle)
-                    
+
                     # Açıların durumunu kontrol et
                     if (75 - tolerance < right_angle < 160 + tolerance) and (75 - tolerance < left_angle < 160 + tolerance):
                         if self.stage is None or self.stage == "down":  # Yukarıya hareket başladı
@@ -107,7 +107,7 @@ class ShoulderPressStrategy(ExerciseStrategy):
                             self.stage = "down"  # Aşağı duruma geç
                             self.counter += 1  # Sayaç artır
                             print(f"Counter: {self.counter}")
-                        
+
                 except Exception as e:
                     print(e)
                 
